608037b7ee8e0a8be25bb8a2b720006545a28a47
Younes SADMI <younes.sadmi@gmail.com>
2016-03-09 12:23:34 +0100

    mes resa

---
+++ js/mesReservations/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.collection-item').click(function(){
+        
+    });
+});
---
+++ views/mesReservations/index.php
@@ -0,0 +1,26 @@
+<div id="button-list">
+    <button class="waves-effect waves-light btn ">A venir</button>
+    <button class="waves-effect waves-light btn">Historique</button>
+</div>
+
+<section id="a-venir">
+    <div class="collection">
+        <a href="#!" class="collection-item">Alvin</a>
+        <a href="#!" class="collection-item active">Alvin</a>
+        <a href="#!" class="collection-item">Alvin</a>
+        <a href="#!" class="collection-item">Alvin</a>
+    </div>
+</section>
+
+<section id="en-cours">
+    aze
+</section>
+
+<section id="historique">
+    <div class="collection">
+        <a href="#!" class="collection-item">Alvin</a>
+        <a href="#!" class="collection-item active">Alvin</a>
+        <a href="#!" class="collection-item">Alvin</a>
+        <a href="#!" class="collection-item">Alvin</a>
+    </div>
+</section>
