3a51b89da04f4da8fd207430d9ba244913334927
Younes SADMI <younes.sadmi@gmail.com>
2016-03-09 16:19:42 +0100

    mes reservations en cours

--- js/mesReservations/index.js
+++ js/mesReservations/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    $('.collection-item').click(function(){
-        
+    var viewDisplayed = '#en-cours';
+    var viewDisplaying = '#en-cours';
+
+
+    $('#button-list button').click(function(){
+        viewDisplayed = viewDisplaying;
+        viewDisplaying = $(this).attr('href');
+        if(viewDisplayed != viewDisplaying){
+            $('section'+viewDisplayed).slideToggle('fast', function(){
+                $('section'+viewDisplaying).slideToggle('fast');
+            });
+        }
+//        console.log(viewDisplayed+' > '+viewDisplaying);
     });
 });
--- views/mesReservations/index.php
+++ views/mesReservations/index.php
@@ -1,9 +1,38 @@
+<h2>Mes réservations</h2>
 <div id="button-list">
-    <button class="waves-effect waves-light btn ">A venir</button>
-    <button class="waves-effect waves-light btn">Historique</button>
+    <button class="waves-effect waves-light btn" href="#a-venir">A venir</button>
+    <!--    <button class="waves-effect waves-light btn" href="#historique">Historique</button>-->
+    <button class="waves-effect waves-light btn" href="#en-cours">En cours</button>
 </div>
 
+<section id="en-cours">
+    <p id="hotel-name">BEST WESTERN PREMIER Opéra Liège</p>
+    <div>
+        <div id="coordonnees">
+            <p><i class="fa fa-calendar"></i> Du <span id="date-start">09/03/2016</span> au <span id="date-end">14/03/2016</span></p>
+            <p><i class="fa fa-map-marker"></i> 9 Rue de Liège, 75009 Paris</p>
+            <p><i class="fa fa-phone"></i> 01 53 72 85 00</p>
+        </div>
+    </div>
+    <div>
+        <img src="<?php echo BASE_URL;?>img/qr_code.png" alt="QR-CODE" id="qr-code">
+        <p style="font-style: italic; margin:0; text-align:center;">Ceci est un QR CODE, il vous permettra de payer directement
+à la fin de votre séjour pour plus de rapidité, vos extras au cours de votre temps dans l’hotel sera automatiquement ajouter à votre note. Ce code sera périmé à votre checkout. Passez un bon moment avec Best Western.</p>
+    </div>
+    <div id="info-resa">
+        <i class="fa fa-bookmark fa-2x"></i>
+        <p style="text-decoration: underline">Plus d'information:</p>
+        <ul>
+            <li>Chambre double</li>
+            <li>2 personne(s)</li>
+            <li>Petit déjeuner inclus</li>
+            <li>5 nuité(s)</li>
+        </ul>
+    </div>
+</section>
+
 <section id="a-venir">
+    a venir
     <div class="collection">
         <a href="#!" class="collection-item">Alvin</a>
         <a href="#!" class="collection-item active">Alvin</a>
@@ -11,12 +40,8 @@
         <a href="#!" class="collection-item">Alvin</a>
     </div>
 </section>
-
-<section id="en-cours">
-    aze
-</section>
-
 <section id="historique">
+    historique
     <div class="collection">
         <a href="#!" class="collection-item">Alvin</a>
         <a href="#!" class="collection-item active">Alvin</a>
