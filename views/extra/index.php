995fb32474dc759b7c132c98f7d381c47d04eadf
Younes SADMI <younes.sadmi@gmail.com>
2016-03-10 12:22:58 +0100

    page d'accueil extra

--- views/extra/index.php
+++ views/extra/index.php
@@ -4,3 +4,24 @@
     </button>
 </a>
 <h2>Extra</h2>
+<div id="extras">
+   <p></p>
+    <table>
+        <tr>
+            <td class="extra top-left">
+                <a href="<?php echo BASE_URL;?>extra/restauration"><i class="fa fa-hashtag fa-4x"></i></a>
+            </td>
+            <td class="extra top-right">
+                <a href="<?php echo BASE_URL;?>extra/hotel"><i class="fa fa-hashtag fa-4x"></i></a>
+            </td>
+        </tr>
+        <tr>
+            <td class="extra bottom-left">
+                <a href="<?php echo BASE_URL;?>extra/activites"><i class="fa fa-hashtag fa-4x"></i></a>
+            </td>
+            <td class="extra bottom-right">
+                <a href="<?php echo BASE_URL;?>extra/restauration"><i class="fa fa-hashtag fa-4x"></i></a>
+            </td>
+        </tr>
+    </table>
+</div>
