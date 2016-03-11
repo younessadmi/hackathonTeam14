1188d05783e617fe0f3e23b980933f49fc37d028
Younes SADMI <younes.sadmi@gmail.com>
2016-03-11 14:14:13 +0100

    menu empty

--- views/layout/header.php
+++ views/layout/header.php
@@ -82,18 +82,6 @@
                         <img src="<?php echo BASE_URL;?>img/logoWebPhone@2x.png" style="width: 73%">
                     </a>
                     <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li>
-                    </ul>
-                    <ul class="side-nav" id="mobile-demo">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li>
-                    </ul>
                 </div>
             </nav>
             <?php } ?>
